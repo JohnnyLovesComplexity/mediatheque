0ad1a3b421531d6f13fde26e9fd4569ecb8d4616
Lea <lea_chemoul@orange.fr>
2019-03-20 09:11:51 +0100

    Control and services

---
+++ src/main/java/com/epul/controle/ControleurOeuvre.java
@@ -0,0 +1,16 @@
+package com.epul.controle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+import com.epul.dao.ServiceAdherentDAO;
+import com.epul.meserreurs.*;
+import com.epul.metier.*;
+
+@Controller
+public class ControleurOeuvre {
+
+}
---
+++ src/main/java/com/epul/dao/ServiceReservationDAO.java
@@ -0,0 +1,16 @@
+package com.epul.dao;
+
+import java.util.*;
+
+import com.epul.metier.*;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import com.epul.meserreurs.*;
+import org.hibernate.Transaction;
+
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+public class ServiceReservationDAO {
+
+}
